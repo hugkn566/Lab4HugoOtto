63d76894fefc71d50390438e35731e3ad9003568
Otto Moen <ottmo242@student.liu.se>
2020-09-10 10:11:23 +0200

    Added the function file

---
+++ Lab4HugoOtto/R/linreg.R
@@ -0,0 +1,48 @@
+#' Perform linear regression with OLS
+#' 
+#' @param formula An object of the class \code{\link[stats]{formula}}.
+#' @param data A data frame.
+
+### linreg ###
+linreg <- function(formula, data){
+  
+  # Setting up the matrices
+  X <- model.matrix(formula, data)
+  y_name <- all.vars(formula, data)
+  Y <- as.matrix(data[y_name[1]])
+  
+  # Regression coefficients
+  beta_hat <- solve(t(X)%*%X) %*% t(X)%*%Y
+  
+  # The fitted values
+  y_hat <- X%*%beta_hat
+  
+  # The Residuals
+  e_hat <- Y-y_hat
+  
+  # The degrees of freedom
+  df <- nrow(data) - ncol(X)
+  
+  # The residual variance
+  sigma2_hat <- as.numeric((t(e_hat)%*%e_hat)/df)
+  
+  # The variance of the regression coefficients
+  var_beta_hat <- sigma2_hat * (solve(t(X)%*%X))
+  
+  # The t-values for each coefficient
+  t_beta <- beta_hat/(sqrt(diag(var_beta_hat)))
+  
+  p <- 2*pt(q=abs(t_beta), df=df, lower.tail = FALSE)
+
+  result <- list(beta_hat=beta_hat,
+                 y_hat=y_hat,
+                 e_hat=e_hat,
+                 df=df,
+                 sigma2_hat=sigma2_hat,
+                 var_beta_hat=var_beta_hat,
+                 t_beta=t_beta,
+                 p=p)
+  class(result) <- "linreg"
+  return(result)
+}
+
