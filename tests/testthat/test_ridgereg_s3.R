34a1fc98b4870f7fea343886da8788b914c884f3
hugkn566 <70522944+hugkn566@users.noreply.github.com>
2020-10-14 12:06:56 +0200

    Final check

--- tests/testthat/test_ridgereg_s3.R
+++ tests/testthat/test_ridgereg_s3.R
@@ -11,7 +11,7 @@ test_that("linreg rejects errounous input", {
 test_that("output is similar to lm.ridge", {
   ridgereg_mod <- ridgereg(Petal.Length~Species, data=iris)
   lm.ridge_mod <- MASS::lm.ridge(Petal.Length~Species, data=iris)
-  expect_equal(round(ridgereg_mod$coef[2], digits = 2), round((lm.ridge_mod$coef[1] / lm.ridge_mod$scales[1]), digits = 2))
-  expect_equal(round(ridgereg_mod$coef[3], digits = 2), round((lm.ridge_mod$coef[2] / lm.ridge_mod$scales[2]), digits = 2))
+  expect_equal(round(ridgereg_mod$coef[2], digits = 2), round((lm.ridge_mod$coef[[1]] / lm.ridge_mod$scales[[1]]), digits = 2))
+  expect_equal(round(ridgereg_mod$coef[3], digits = 2), round((lm.ridge_mod$coef[[2]] / lm.ridge_mod$scales[[2]]), digits = 2))
 })
 
